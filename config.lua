4db70da580bfdcea18f39e3d265ba062ef2180e5
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2024-12-28 19:38:22 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,17 @@
 Config = {}
 
+-- Enable or disable the case
+Config.EnableCase = true
+
 -- Set Wait tick rate
 
 Config.Wait = 1000    -- 1000 = 1 second
 
 -- Enable or disable the use of the case
-Config.EnableCase = true
-Config.CanSprintWithCase = false
+Config.CanSprintWithCase = true
+Config.CanHoldWeaponWithCase = true
+
+-- Enable or disable the case
 
 -- Save amount of money to show the case
 Config.Amount = 10
